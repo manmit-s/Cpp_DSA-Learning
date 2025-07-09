f4bd07cba0ee08ea61e9dc3312313da93851fe70
manmit <manmitsamal84@gmail.com>
2025-07-09 22:04:59 +0530

    reverse words in string leetcode

---
+++ rev_words_in_string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string reverseWords(string s){
+    int n = s.length();
+
+    string ans = "";
+    reverse(s.begin(), s.end());
+    for(int i = 0; i < n; i++){
+        string word = "";
+        while(i < n && s[i] != ' '){ //to separate individual word
+            word += s[i];
+            i++;
+        }
+        reverse(word.begin(), word.end());
+        if(word.length() > 0){
+            ans = ans  + " " + word;
+        }
+    }
+    return ans.substr(1);
+}
+
+int main(){
+    string s = "the sky is blue";
+    cout<<reverseWords(s)<<endl;
+    return 0;
+}
