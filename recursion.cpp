0e20c8efdda8f6bc536f4243c19f5554a3cfe2ba
manmit <manmitsamal84@gmail.com>
2025-12-02 14:35:59 +0530

    recursion

---
+++ recursion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void printNum(int n){ // print from n to 1
+    if(n == 1){
+        cout<<"1"<<endl;
+        return;
+    }
+    cout<<n<<" ";
+    printNum(n - 1);
+}
+
+int factorial(int n){
+    if(n == 1){
+        return 1;
+    }
+    return factorial(n - 1) * n;
+}
+
+int main(){
+    printNum(10);
+    cout<<factorial(5)<<endl;
+    return 0;
+}
