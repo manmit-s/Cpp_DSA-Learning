65012f42ba03b3b8859081fb1d0007c355fe1a54
manmit <manmitsamal84@gmail.com>
2026-01-10 20:08:23 +0530

    leetcode easy qs used unordered map (hash map)

---
+++ intersection_of_array_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+    unordered_map<int, int> freq;
+    for(int x : nums1){
+        freq[x]++;
+    }
+
+    vector<int> result;
+    for(int x : nums2){
+        if(freq[x] > 0){
+            result.push_back(x);
+        }
+        freq[x]--;
+    }
+    return result;
+}
+
+int main(){
+    
+    return 0;
+}
