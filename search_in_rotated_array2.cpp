4db95d0e70b7a11ca3ddcd41a1ed422c5fd4c99b
manmit <manmitsamal84@gmail.com>
2026-01-17 20:52:51 +0530

    Leetcode medium search rotated array part 2

---
+++ search_in_rotated_array2.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/* Unconventional Test Cases from AI so that I dont fkup */
+//T1 (Passed)
+// nums = [2,2,2,2,2,2,2]
+// target = 3
+
+//T2
+// nums = [3,1,1,1,2,2,2]
+// target = 3
+
+//T3
+// nums = [1,1,1,3,1]
+// target = 3
+
+//T4
+// nums = [1,1,3,1]
+// target = 3
+
+//T5
+// nums = [1]
+// target = 0
+
+//T6 (Failed) -> (Passed)
+// nums = [1,0,1,1,1]
+// target = 0
+
+
+bool search(vector<int>& nums, int target) {
+    int s = 0, e = nums.size() - 1;
+
+    while(s <= e){
+        int mid = s + (e - s)/2;
+
+        if(nums[mid] == target) return true;
+
+        //Handle Duplicates for T6
+        if(nums[s] == nums[mid] && nums[mid] == nums[e]){
+            s++, e--;
+        }
+
+        else if(nums[s] <= nums[mid]){
+            if(nums[s] <= target && nums[mid] >= target){
+                e = mid - 1;
+            }
+            else s = mid + 1;
+        }
+        else{
+            if(nums[e] >= target && nums[mid] <= target){
+                s = mid + 1;
+            }
+            else e = mid - 1;
+        }
+    }
+    return false; //should never execute
+}
+
+int main(){
+    vector<int> nums = {1,0,1,1,1};
+    int target = 0;
+
+    cout<<search(nums, target);
+
+    return 0;
+}
