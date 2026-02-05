7bd4e958d75729e029ed584a56f0befe92473beb
manmit <manmitsamal84@gmail.com>
2026-02-05 22:02:49 +0530

    LC medium Must Revisit

---
+++ int_to_roman.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+//For Future ME!!
+// yeh toh maine soln dekha toh samjha ki hash map kese implement krna hai otherwise jab hashmap nhi dekha toh mai esa soch rha tha ki jese eg: 58 hai toh ek helper fn se mai vector return karunga -> (8, 50) as in expanded form and is vector se popback krte jaaunga to see woh number kis range mai aata hai uske according if else ladder lagega pura
+
+string intToRoman(int num){
+    map<int, string, greater<int>> intAndRoman = {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"},
+        {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+    };
+
+    string ans = "";
+
+    for(auto &m : intAndRoman){
+        if(num / m.first){
+            int count = num/m.first;
+            for(int i = 1; i <= count; i++) ans.append(m.second);
+            num = num % m.first;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    cout<<intToRoman(58);
+    return 0;
+}
