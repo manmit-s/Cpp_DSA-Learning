124adc88a06411984ef3c46d022066ec6b9bb4cf
manmit <manmitsamal84@gmail.com>
2026-02-05 23:09:02 +0530

    optimization by using pair

--- int_to_roman.cpp
+++ int_to_roman.cpp
@@ -7,11 +7,15 @@ using namespace std;
 //For Future ME!!
 // yeh toh maine soln dekha toh samjha ki hash map kese implement krna hai otherwise jab hashmap nhi dekha toh mai esa soch rha tha ki jese eg: 58 hai toh ek helper fn se mai vector return karunga -> (8, 50) as in expanded form and is vector se popback krte jaaunga to see woh number kis range mai aata hai uske according if else ladder lagega pura
 
+// Update hashmap pe computational time zyada lag rha tha so I used pair instead it is far more lightweight and takes O(1) to access elements whereas map takes O(log n) time
+
 string intToRoman(int num){
-    map<int, string, greater<int>> intAndRoman = {
-        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"},
-        {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+    pair<int, string> intAndRoman[] = {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
     };
+
 
     string ans = "";
 
