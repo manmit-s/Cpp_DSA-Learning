f3d6d96e59a1ccb7bd9029f3fdf364d8eb96c232
manmit <manmitsamal84@gmail.com>
2026-03-07 20:50:58 +0530

    cleaned the code

--- trim_trailing_vowels.cpp
+++ trim_trailing_vowels.cpp
@@ -15,29 +15,6 @@ string removeTrailingVowels(string s) {
     return s.substr(0, i + 1); // keep everything up to i
 }
 
-
-// string trimTrailingVowels(string s) {
-//     string ans = "";
-//     bool flag = false;
-//     for(int i = 1; i < s.size(); i++){
-//         //character is repeated vowel
-//         if(isVowel(s[i]) && flag){
-//             s.pop_back();
-//             flag = !flag; //turn flag false
-//         }
-//         // character is not vowel
-//         else if(!isVowel(s[i])){
-//             s.push_back(s[i]);
-//             flag = false; //turn flag false
-//         }
-//         // character is vowel
-//         else{
-//             s.push_back(s[i]);
-//             flag = !flag //turn flag true
-//         }
-//     }
-// }
-
 int main(){
     
     return 0;
