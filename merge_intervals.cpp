e31d523094dfc7efc39819739430dd871fc2916f
manmit <manmitsamal84@gmail.com>
2026-03-14 13:50:51 +0530

    initially there was a logic flaw

---
+++ merge_intervals.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end());
+
+    vector<vector<int>> ans;
+    ans.push_back(intervals[0]);
+    for(int i = 0; i < intervals.size(); i++){
+        if(ans.back()[1] >= intervals[i][0]){
+            ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+        }
+        else{
+            ans.push_back(intervals[i]);
+        }
+    }
+    return ans;
+}
+
+int main(){
+    
+    return 0;
+}
