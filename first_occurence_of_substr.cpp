31eea792e627e541b2b26b35ac18ae0a31429cf8
manmit <manmitsamal84@gmail.com>
2026-03-17 22:00:53 +0530

    LC easy find needle from the  haystack

---
+++ first_occurence_of_substr.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int strStr(string haystack, string needle) {
+    int n = haystack.size(), m = needle.size();
+
+    for(int i = 0; i <= n-m; i++){
+        if(haystack.substr(i, m) == needle){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    
+    return 0;
+}
