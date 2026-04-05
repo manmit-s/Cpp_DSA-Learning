8fdefd2a0307569583fe2948810555ee2123a1ed
manmit <manmitsamal84@gmail.com>
2026-04-05 14:08:11 +0530

    LC_452 greedy approach required

---
+++ LC_452_min_arrows_to_burst_balloons.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int findMinArrowShots(vector<vector<int>>& points) {
+    if(points.empty()) return 0;
+
+    sort(points.begin(), points.end(), [](auto &a, auto &b){
+        return a[1] < b[1];
+    });
+
+    int arrows = 1;
+    int end = points[0][1];
+
+    for(int i = 1; i < points.size(); i++){
+        if(end < points[i][0]){
+            arrows++;
+            end = points[i][1];
+        }
+    }
+
+    return arrows;
+}
+
+int main(){
+    
+    return 0;
+}
