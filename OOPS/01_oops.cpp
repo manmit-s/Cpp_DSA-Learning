a814690a4cdb37ef654b8bc9afe4fd96e9c2807c
manmit <manmitsamal84@gmail.com>
2026-04-12 01:35:11 +0530

    Lesson 1 Completed OOPS: class and objects, Encapsulation

---
+++ OOPS/01_oops.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher{
+    //Access Modifiers -- private, public, protected
+    public:
+        //Properties or Attributes
+        string name;
+        string dept;
+        string subject;
+        double salary;
+
+        //Methods or Member Functions
+        void changeDept(string newDept){
+            dept = newDept;
+        }
+};
+
+int main(){
+    Teacher t1; //Object of Teacher Class
+    t1.name = "Manmit";
+    t1.dept = "Artificial Intelligence";
+    t1.subject = "Generative AI";
+    t1.salary = 600000;
+    
+    cout<<t1.dept;
+    return 0;
+}
