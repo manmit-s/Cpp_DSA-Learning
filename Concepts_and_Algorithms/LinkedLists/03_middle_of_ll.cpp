617e41cfcdc186353d25884b030538aee340de42
manmit <manmitsamal84@gmail.com>
2026-04-14 21:26:11 +0530

    finding middle of LL

---
+++ Concepts_and_Algorithms/LinkedLists/03_middle_of_ll.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+
+        Node(int val){
+            data = val;
+            next = NULL;
+        }
+
+};
+
+class List{
+    Node* head;
+    Node* tail;
+
+    public:
+        List(){
+            head = tail = NULL;
+        }
+
+        void push_back(int val){
+            Node* newNode = new Node(val);
+            if(tail == NULL){
+                head = tail = newNode;
+                return;
+            }
+            else{
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+
+        int middleNode(){
+            int n = nodeCount();
+            Node* temp = head;
+            for(int i = 0; i < n/2; i++){
+                temp = temp->next;
+            }
+            return temp->data;
+        }
+
+        int nodeCount(){
+            Node* temp = head;
+            int count = 0;
+            while(temp != NULL){
+                temp = temp->next;
+                count++;
+            }
+            return count;
+        }
+
+        void display(){
+            Node* temp = head;
+            while(temp != NULL){
+                cout<<temp->data<<"\t";
+                temp = temp->next;
+            }
+            cout<<endl;
+        }
+};
+
+int main(){
+    List ll;
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_back(4);
+
+    ll.display();
+    cout<<ll.middleNode()<<endl;
+    return 0;
+}
