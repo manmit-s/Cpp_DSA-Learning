c5b4f3119ee05b8b91cdb87f78ea45b3b5962fdd
manmit <manmitsamal84@gmail.com>
2026-05-20 17:18:21 +0530

    LC 138 medium LL qs

---
+++ LeetCode/LC_138_copy_list_with_random_pointer.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+        Node *newHead = new Node(head->val);
+        Node *oldTemp = head->next;
+        Node *newTemp = newHead;
+        unordered_map<Node*, Node*> map;
+        map[head] = newHead;
+
+        while(oldTemp){
+            Node *copyNode = new Node(oldTemp->val);
+            map[oldTemp] = copyNode;
+            newTemp->next = copyNode;
+            oldTemp = oldTemp->next;
+            newTemp = newTemp->next;
+        }
+
+        oldTemp = head; newTemp = newHead;
+        while(oldTemp){
+            newTemp->random = map[oldTemp->random];
+            oldTemp = oldTemp->next;
+            newTemp = newTemp->next;
+        }
+        
+        return newHead;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
