15cf66105382c593cd60dfea48e971ff6acef070
manmit <manmitsamal84@gmail.com>
2026-05-28 22:04:36 +0530

    LC 3516 why this qs even exists in DSA platform??

---
+++ LeetCode/LC_3516_closest_person.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        if(abs(x-z) < abs(y-z)) return 1;
+        else if(abs(x-z) > abs(y-z)) return 2;
+        else return 0;
+    }
+};  
+
+
+int main(){
+    
+    return 0;
+}
