106335657e2f449bed0548afa6ba1bce3480762d
manmit <manmitsamal84@gmail.com>
2026-07-02 20:59:02 +0530

    LC 1550 easy problem no xhit

---
+++ LeetCode/LC_1550_three_consecutive_odds.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        for (int i = 0; i + 2 < arr.size(); i++) {
+            if (arr[i] % 2 != 0 && 
+                arr[i+1] % 2 != 0 && 
+                arr[i+2] % 2 != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
