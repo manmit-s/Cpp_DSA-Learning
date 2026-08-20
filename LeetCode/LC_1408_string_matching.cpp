94047d9540027f3334168e6185bc171e0e2b8abd
manmit <manmitsamal84@gmail.com>
2026-08-20 22:13:44 +0530

    LC_1408_ string matching

---
+++ LeetCode/LC_1408_string_matching.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> result;
+        int n = words.size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && words[j].find(words[i]) != string::npos) {
+                    result.push_back(words[i]);
+                    break; 
+                }
+            }
+        }
+        
+        return result;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
